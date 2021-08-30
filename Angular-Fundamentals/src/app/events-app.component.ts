ad46e384719feef9ec45f6d263acbfe36f18e3b9
AkshaykumarRjpt <41144715+AkshaykumarRjpt@users.noreply.github.com>
2021-08-30 15:43:16 +0530

    added another component (event-list.component)

--- Angular-Fundamentals/src/app/events-app.component.ts
+++ Angular-Fundamentals/src/app/events-app.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'events-app',
   template:`<h2> hello akshay<h2>
-  <img src = "/assets/images/basic-shield.png"/>
+  
   <events-list></events-list>
   `
 })
