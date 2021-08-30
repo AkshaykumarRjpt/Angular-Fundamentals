bf11cfd1f3ca7e0eb20488209a91caab0654c6a1
AkshaykumarRjpt <41144715+AkshaykumarRjpt@users.noreply.github.com>
2021-08-30 14:47:36 +0530

    First component

--- Angular-Fundamentals/src/app/app.module.ts
+++ Angular-Fundamentals/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponents } from './events/events-list.components';
 
-import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventsListComponents
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
