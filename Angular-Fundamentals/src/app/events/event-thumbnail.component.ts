2a5f67572759c3646c0bbf34ec33b0a8cfc04937
AkshaykumarRjpt <41144715+AkshaykumarRjpt@users.noreply.github.com>
2021-08-31 10:50:17 +0530

    Looping with *ngFor to have multiple event thumbnail

--- Angular-Fundamentals/src/app/events/event-thumbnail.component.ts
+++ Angular-Fundamentals/src/app/events/event-thumbnail.component.ts
@@ -8,11 +8,11 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     <h2> {{event.name}}</h2>
     <div> Date: {{event.date}}</div>
     <div> time: {{event.time}}</div>
-    <div> Price: /$ {{event.price}}</div>
+    <div> Price: \$ {{event.price}}</div>
 
 <div>
-   <span>Location:{{event.location.address}}</span>
-   <span class = "pad-left"> {{event.location.city}}, {{event.location.country}}</span>
+   <span>Location:{{event.location?.address}}</span>
+   <span class = "pad-left"> {{event.location?.city}}, {{event.location?.country}}</span>
 </div>
 </div>`,
 styles: [ `.pad-left {margin-left: 10px;}
