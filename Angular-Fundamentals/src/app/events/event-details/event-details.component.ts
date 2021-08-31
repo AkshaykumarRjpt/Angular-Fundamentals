4e9a7cbb2d893c517f9a7defa064a95c20663384
AkshaykumarRjpt <41144715+AkshaykumarRjpt@users.noreply.github.com>
2021-08-31 18:06:47 +0530

    accessing route parameter from ActivatedRoute.Snapshot.params

--- Angular-Fundamentals/src/app/events/event-details/event-details.component.ts
+++ Angular-Fundamentals/src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { EventService } from "../shared/event.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     templateUrl:'./event-details.component.html',
@@ -8,14 +9,15 @@ import { EventService } from "../shared/event.service";
 })
 export class EventDetailsComponent implements OnInit {
     
-    constructor(private eventService: EventService){
+    constructor(private eventService: EventService, private route: ActivatedRoute){
 
     }
     
     event:any
 
     ngOnInit() {
-      this.event =  this.eventService.getEvent(1)
+        console.log(this.route.snapshot.params['id'])
+      this.event =  this.eventService.getEvent(+this.route.snapshot.params['id'])
 
     }
 
