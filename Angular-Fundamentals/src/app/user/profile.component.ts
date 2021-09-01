5af88122eb7bb4f5a96747de2bafc7f9f451c6d1
AkshaykumarRjpt <41144715+AkshaykumarRjpt@users.noreply.github.com>
2021-09-01 19:02:53 +0530

    Added Validation for Reactive Form

--- Angular-Fundamentals/src/app/user/profile.component.ts
+++ Angular-Fundamentals/src/app/user/profile.component.ts
@@ -1,33 +1,52 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { AuthService } from './auth.service'
 import { Router } from '@angular/router'
 
 @Component({
   selector: 'profile',
   templateUrl: './profile.component.html',
-  styleUrls: []
+  styles: [`
+    em {float:right; color:#E05C65; padding-left:10px}
+    .error input{background-color:#E3C3C5;}
+    .error :: -webkit-input-placeholder {color: #999}
+  `]
 })
 export class ProfileComponent implements OnInit {
  
   constructor(private auth:AuthService, private route:Router){
 
   }
+  firstname!:FormControl
+  lastname!: FormControl
   profileForm!:FormGroup
  
   ngOnInit(): void {
-    let firstname = new FormControl(this.auth.currentUser.firstName)
-    let lastname = new FormControl(this.auth.currentUser.lastName)
+    this.firstname = new FormControl(this.auth.currentUser.firstName, Validators.required)
+    this.lastname = new FormControl(this.auth.currentUser.lastName, Validators.required)
 
     this.profileForm = new FormGroup ({
-      firstName: firstname,
-      lastName: lastname
+      firstName: this.firstname,
+      lastName: this.lastname
     })
   }
 
+  validateFirstName(){
+    return (this.firstname.invalid &&
+      this.firstname.touched)
+  }
+
+  validateLastName(){
+    return (this.lastname.invalid &&
+      this.lastname.touched)
+  }
+
   saveProfile(formValue:any){
+    if(this.profileForm.valid)
+    {
     this.auth.updateCurrentUser(formValue.firstName, formValue.lastName)
     this.route.navigate(['events'])
+    }
   }
 
   onCancel(){
