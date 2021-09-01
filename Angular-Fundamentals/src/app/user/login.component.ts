0bfa2ea59cc9678ba606f2ae15cf2ad8b2df2354
AkshaykumarRjpt <41144715+AkshaykumarRjpt@users.noreply.github.com>
2021-09-01 16:40:49 +0530

    Added Validation for template based form

--- Angular-Fundamentals/src/app/user/login.component.ts
+++ Angular-Fundamentals/src/app/user/login.component.ts
@@ -3,7 +3,10 @@ import { Router } from "@angular/router";
 import { AuthService } from "./auth.service";
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styles:[`
+    em {float:right; color:#E05C65; padding-left:10px}
+    `]
 })
 export class LoginComponent{
 
@@ -12,6 +15,7 @@ export class LoginComponent{
     }
     userName:any
     password:any
+    mouseoverLogin!:boolean
 
     login(formValues:any){
         this.authService.loginUser(formValues.userName, formValues.password)
@@ -23,7 +27,7 @@ export class LoginComponent{
     }
 
     cancel(){
-        
+         
         this.router.navigate(['events'])
     }
    
